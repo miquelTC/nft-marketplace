9d1fe491c6a354ac50b078b34dbfd47dde18de20
miquelTC <miqueltc@DESKTOP-G55QVS5.localdomain>
2021-08-02 10:48:16 +0200

    create a smart contract and add some erc721 functionalities on it

---
+++ src/contracts/NFTCollection.sol
@@ -0,0 +1,44 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
+
+/*
+** Mint NFT
+** Buy NFT
+** Change price
+*/
+
+contract NFTCollection is ERC721, ERC721Enumerable {
+  string[] public tokenURIs;
+  mapping(string => bool) _tokenURIExists;
+  mapping(uint => string) _tokenIdToTokenURI;
+
+  constructor() 
+    ERC721("mTC Collection", "mTC") 
+  {
+  }
+
+  function _beforeTokenTransfer(address from, address to, uint256 tokenId) internal override(ERC721, ERC721Enumerable) {
+    super._beforeTokenTransfer(from, to, tokenId);
+  }
+
+  function supportsInterface(bytes4 interfaceId) public view override(ERC721, ERC721Enumerable) returns (bool) {
+    return super.supportsInterface(interfaceId);
+  }
+
+  function tokenURI(uint256 tokenId) public override view returns (string memory) {
+    require(_exists(tokenId), "ERC721Metadata: URI query for nonexistent token");
+    return _tokenIdToTokenURI[tokenId];
+  }
+
+  function safeMint(string memory _tokenURI) public {
+    require(!_tokenURIExists[_tokenURI], 'The token URI should be unique');
+    tokenURIs.push(_tokenURI);    
+    uint _id = tokenURIs.length;
+    _tokenIdToTokenURI[_id] = _tokenURI;
+    _safeMint(msg.sender, _id);
+    _tokenURIExists[_tokenURI] = true;
+  }
+}
